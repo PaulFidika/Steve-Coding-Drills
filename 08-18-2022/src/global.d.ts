dea8c47f86e206726b5adbefde45ea4e58e32889
Paul Fidika <paul@fidika.com>
2022-08-19 10:21:24 -0500

    I cannot for the life of me get fucking global types to work

---
+++ 08-18-2022/src/global.d.ts
@@ -0,0 +1,6 @@
+export {};
+
+declare type TestType = {
+  a: number;
+  b: number;
+};
---
+++ 08-18-2022/src/index.ts
@@ -0,0 +1,9 @@
+export {};
+
+console.log('hello there everyone');
+
+function thing() {
+  const abc: TestType = { a: 5, b: 19 };
+}
+
+thing();
